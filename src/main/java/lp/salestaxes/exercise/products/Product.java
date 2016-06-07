c4db1cb51f7a70bf56e030bb3ec513c53a842115
lpand <luca1.pandini@gmail.com>
2016-06-07 21:54:12 +0200

    outline of product building

--- src/main/java/lp/salestaxes/exercise/products/Product.java
+++ src/main/java/lp/salestaxes/exercise/products/Product.java
@@ -2,4 +2,22 @@ package lp.salestaxes.exercise.products;
 
 public interface Product {
 
+	/**
+	 * Name or description of the product.
+	 * @return name or description.
+	 */
+	String getName();
+	
+	/**
+	 * Product net price.
+	 * 
+	 * @return price.
+	 */
+	double getPrice();
+	
+	/**
+	 * @return the quantity of this product.
+	 */
+	int getQty();
+	
 }
