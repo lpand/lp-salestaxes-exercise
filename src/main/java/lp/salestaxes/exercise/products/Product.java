1e91736085309c0cd4ad742923bcc0a1fc903650
lpand <luca1.pandini@gmail.com>
2016-06-08 18:06:11 +0200

    add categories to a product

--- src/main/java/lp/salestaxes/exercise/products/Product.java
+++ src/main/java/lp/salestaxes/exercise/products/Product.java
@@ -1,17 +1,29 @@
 package lp.salestaxes.exercise.products;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Product {
 	
 	private final String name;
 	private final double cost;
 	private final int qty;
+	private final Set<Category> cats;
 	
-	public Product(String name, double cost, int quantity) {
+	public Product(String name, double cost, int quantity, Collection<Category> cats) {
 		this.name = name;
 		this.cost = cost * quantity;
 		this.qty = quantity;
+		this.cats = new HashSet<Category>(cats);
 	}
-
+	
+	public Product(String name, double cost, int quantity, Category category) {
+		this(name, cost, quantity, Arrays.asList(category));
+	}
+	
 	/**
 	 * Name or description of the product.
 	 * @return name or description.
@@ -34,6 +46,14 @@ public class Product {
 	 */
 	public int getQty() {
 		return qty;
+	}
+
+	/**
+	 * Categories this product belongs to.
+	 * @return Categories this product belongs to.
+	 */
+	public Set<Category> getCategories() {
+		return cats;
 	}
 	
 }
--- src/test/java/lp/salestaxes/exercise/products/ProductTest.java
+++ src/test/java/lp/salestaxes/exercise/products/ProductTest.java
@@ -1,7 +1,14 @@
 package lp.salestaxes.exercise.products;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -9,13 +16,13 @@ import org.junit.Test;
 public class ProductTest {
 
 	Product prod;
-	final String name = "box of chocolates";
+	final String name = "digital book To Kill a Mockingbird";
 	final int qty = 3;
 	final double unitCost = 3.5;
 	
 	@Before
 	public void beforeEach() {
-		prod = new Product(name, unitCost, qty);
+		prod = new Product(name, unitCost, qty, EnumSet.of(Category.BOOK, Category.DIGITAL));
 	}
 	
 	@Test
@@ -23,6 +30,14 @@ public class ProductTest {
 		assertThat(prod.getName(), is(name));
 		assertThat(prod.getQty(), is(qty));
 		assertThat(prod.getCost(), is(qty * unitCost));
+		assertThat(prod.getCategories(), contains(Category.BOOK, Category.DIGITAL));
+	}
+	
+	@Test
+	public void categoryChangesDoesNotAffectTheProduct() {
+		Set<Category> cats = prod.getCategories();
+		cats.clear();
+		assertThat(prod.getCategories(), not(empty()));
 	}
 
 }
